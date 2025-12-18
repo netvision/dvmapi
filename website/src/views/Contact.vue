57cc9e3af47d49a88451d9f4c2021a2bf4c2e0e4
Rakesh Jangid <rakesh@jangid.co.in>
2025-12-18 10:23:09 +0530

    typescript error in contact page fixed

--- website/src/views/Contact.vue
+++ website/src/views/Contact.vue
@@ -262,7 +262,7 @@
                 width="100%" 
                 height="100%" 
                 style="border:0;" 
-                allowfullscreen="" 
+                allowfullscreen
                 loading="lazy" 
                 referrerpolicy="no-referrer-when-downgrade"
               ></iframe>
